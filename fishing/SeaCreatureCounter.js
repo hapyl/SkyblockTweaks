67f8a03c7f8aeb3e56b7514da6e6dd1312bd4af5
hapyl <deojoff@gmail.com>
2023-07-19 19:41:30 +0600

    Fix shark rarity.

--- fishing/SeaCreatureCounter.js
+++ fishing/SeaCreatureCounter.js
@@ -1,16 +1,42 @@
 import { info, success, error, debug } from "../Utils";
 
 var RARITY = {
-    1: "&f&lCOMMON",
-    2: "&a&lUNCOMMON",
-    3: "&9&lRARE",
-    4: "&5&lEPIC",
-    5: "&6&lLEGENDARY",
-    6: "&d&lMYTHIC",
-    7: "&d&lDIVINE",
-    8: "&c&lSPECIAL",
-    9: "&c&lVERY SPECIAL",
-    10: "&4&lADMIN",
+    COMMON: {
+        color: "&f",
+        name: "COMMON"
+    },
+    UNCOMMON: {
+        color: "&a",
+        name: "UNCOMMON"
+    },
+    RARE: {
+        color: "&9",
+        name: "RARE"
+    },
+    EPIC: {
+        color: "&5",
+        name: "EPIC"
+    },
+    LEGENDARY: {
+        color: "&6",
+        name: "LEGENDARY"
+    },
+    MYTHIC: {
+        color: "&d",
+        name: "MYTHIC"
+    },
+    DIVINE: {
+        color: "&d",
+        name: "DIVINE"
+    },
+    SPECIAL: {
+        color: "&c",
+        name: "SPECIAL"
+    },
+    VERY_SPECIAL: {
+        color: "&c",
+        name: "VERY SPECIAL"
+    }
 };
 
 var SEA_CREATURES = {
@@ -18,197 +44,197 @@ var SEA_CREATURES = {
     SQUID: {
         name: "Squid",
         message: "A Squid appeared.",
-        rarity: 1,
+        rarity: "COMMON",
     },
     SEA_WALKER: {
         name: "Sea Walker",
         message: "You caught a Sea Walker.",
-        rarity: 1,
+        rarity: "COMMON",
     },
     NIGHT_SQUID: {
         name: "Night Squid",
         message: "Pitch darkness reveals a Night Squid.",
-        rarity: 1,
+        rarity: "COMMON",
     },
     SEA_GUARDIAN: {
         name: "Sea Guardian",
         message: "You stumbled upon a Sea Guardian.",
-        rarity: 1,
+        rarity: "COMMON",
     },
     SEA_WITCH: {
         name: "Sea Witch",
         message: "It looks like you've disrupted the Sea Witch's brewing session. Watch out, she's furious!",
-        rarity: 2,
+        rarity: "UNCOMMON",
     },
     SEA_ARCHER: {
         name: "Sea Archer",
         message: "You reeled in a Sea Archer.",
-        rarity: 2,
+        rarity: "UNCOMMON",
     },
     RIDER_OF_THE_DEEP: {
         name: "Rider Of The Deep",
         message: "The Rider of the Deep has emerged.",
-        rarity: 2,
+        rarity: "UNCOMMON",
     },
     CATFISH: {
         name: "Catfish",
         message: "Huh? A Catfish!",
-        rarity: 3,
+        rarity: "RARE",
     },
     CARROT_KING: {
         name: "Carrot King",
         message: "Is this even a fish? It's the Carrot King!",
-        rarity: 3,
+        rarity: "RARE",
     },
     SEA_LEECH: {
         name: "Sea Leech",
         message: "Gross! A Sea Leech",
-        rarity: 3,
+        rarity: "RARE",
     },
     GUARDIAN_DEFENDER: {
         name: "Guardian Defender",
         message: "You've discovered a Guardian Defender of the sea.",
-        rarity: 4,
+        rarity: "EPIC",
     },
     DEEP_SEA_PROTECTOR: {
         name: "Deep Sea Protector",
         message: "You have awoken the Deep Sea Protector, prepare for a battle!",
-        rarity: 4,
+        rarity: "EPIC",
     },
     WATER_HYDRA: {
         name: "Water Hydra",
         message: "The Water Hydra has come to test your strength",
-        rarity: 5,
+        rarity: "LEGENDARY",
     },
     THE_SEA_EMPEROR: {
         name: "The Sea Emperor",
         message: "Sea Emperor arises from the depths.",
-        rarity: 5,
+        rarity: "LEGENDARY",
     },
     AGARIMOO: {
         name: "Agarimoo",
         message: "Your Chumcap Bucket trembles, it's an Agarimoo.",
-        rarity: 3,
+        rarity: "RARE",
     },
     OASIS_RABBIT: {
         name: "Oasis Rabbit",
         message: "An Oasis Rabbit appears from the water",
-        rarity: 2,
+        rarity: "UNCOMMON",
     },
     OASIS_SHEEP: {
         name: "Oasis Sheep",
         message: "An Oasis Sheep appears from the water",
-        rarity: 2,
+        rarity: "UNCOMMON",
     },
     // Crystal Hollows
     WATER_WORM: {
         name: "Water Worm",
         message: "A Water Worm surfaces!",
-        rarity: 3,
+        rarity: "RARE",
     },
     POISONED_WATER_WORM: {
         name: "Poisoned Water Worm",
         message: "A Poisoned Water Worm surfaces!",
-        rarity: 3,
+        rarity: "RARE",
     },
     ZOMBIE_MINER: {
         name: "Zombie Miner",
         message: "A Zombie miner surfaces!",
-        rarity: 5,
+        rarity: "LEGENDARY",
     },
     // Spooky
     SCARECROW: {
         name: "Scarecrow",
         message: "Phew! It's only a Scarecrow.",
-        rarity: 1,
+        rarity: "COMMON",
     },
     NIGHTMARE: {
         name: "Nightmare",
         message: "You hear trotting from beneath the waves, you caught a Nightmare.",
-        rarity: 3,
+        rarity: "RARE",
     },
     WEREWOLF: {
         name: "Werewolf",
         message: "It must be a full moon, a Werewolf appears.",
-        rarity: 4,
+        rarity: "EPIC",
     },
     PHANTOM_FISHERMAN: {
         name: "Phantom Fisherman",
         message: "The spirit of a long lost Phantom Fisherman has come to haunt you.",
-        rarity: 5,
+        rarity: "LEGENDARY",
     },
     GRIM_REAPER: {
         name: "Grim Reaper",
         message: "This can't be! The manifestation of death himself!",
-        rarity: 5,
+        rarity: "LEGENDARY",
     },
     // Winter
     FROZEN_STEVE: {
         name: "Frozen Steve",
         message: "Frozen Steve fell into the pond long ago, never to resurface...until now!",
-        rarity: 1,
+        rarity: "COMMON",
     },
     FROSTY_THE_SNOWMAN: {
         name: "Frosty The Snowman",
         message: "It's a snowman! He looks harmless",
-        rarity: 1,
+        rarity: "COMMON",
     },
     GRINCH: {
         name: "Grinch",
         message: "The Grinch stole Jerry's Gifts...get them back!",
-        rarity: 2,
+        rarity: "UNCOMMON",
     },
     YETI: {
         name: "Yeti",
         message: "What is this creature!?",
-        rarity: 5,
+        rarity: "LEGENDARY",
     },
     NUTCRACKER: {
         name: "Nutcracker",
         message: "You found a forgotten Nutcracker laying beneath the ice.",
-        rarity: 3,
+        rarity: "RARE",
     },
     REINDRAKE: {
         name: "Reindrake",
         message: "A Reindrake forms from the depths.",
-        rarity: 5,
+        rarity: "LEGENDARY",
     },
     // Mammy (Marina)
     NURSE_SHARK: {
         name: "Nurse Shark",
         message: "A tiny fin emerges from the water, you've caught a Nurse Shark.",
-        rarity: 6,
+        rarity: "UNCOMMON",
     },
     BLUE_SHARK: {
         name: "Blue Shark",
         message: "You spot a fin as blue as the water it came from, it's a Blue Shark.",
-        rarity: 6,
+        rarity: "RARE",
     },
     TIGER_SHARK: {
         name: "Tiger Shark",
         message: "A striped beast bounds from the depths, the wild Tiger Shark!",
-        rarity: 6,
+        rarity: "EPIC",
     },
     GREAT_WHITE_SHARK: {
         name: "Great White Shark",
         message: "Hide no longer, a Great White Shark has tracked your scent and thirsts for your blood!",
-        rarity: 6,
+        rarity: "LEGENDARY",
     },
     // Crystal Hollows Lava
     FLAMING_WORM: {
         name: "Flaming Worm",
         message: "A flaming worm surfaces from the depths!",
-        rarity: 3,
+        rarity: "RARE",
     },
     LAVA_BLAZE: {
         name: "Lava Blaze",
         message: "A Lava Blaze has surfaced from the depths!",
-        rarity: 4,
+        rarity: "EPIC",
     },
     LAVA_PIGMAN: {
         name: "Lava Pigman",
         message: "A Lava Pigman arose from the depths!",
-        rarity: 4,
+        rarity: "EPIC",
     },
 };
 
@@ -243,26 +269,35 @@ register("command", (arg) => {
             return;
         }
 
-        success(`You caught a total of ${countTotalSeaCreatures()} sea creatures!`);
+        let fishingTime = formatTimeFromMillis(Date.now() - firstCatch);
 
-        caughtSeaCreatures.forEach((count, key) => {
+        success(`You caught ${countTotalSeaCreatures()} sea creatures! &8(${fishingTime})`);
+
+        const array = Array.from(caughtSeaCreatures);
+        array.sort((a, b) => b[1] - a[1]);
+
+        let toDisplay = new Map(array);
+
+        toDisplay.forEach((count, key) => {
             const creature = SEA_CREATURES[key];
             const rarity = getRarity(creature.rarity);
 
-            info(`  ${rarity} &2${creature.name} &8x${count}`);
+            info(`  &7${count} x ${rarity.color}${creature.name}`)
         });
+
+        success("Use &e/scc reset&a to reset the counter.");
 
         return;
     }
 
-    if (arg === "clear") {
+    if (arg === "reset") {
         caughtSeaCreatures = new Map();
         firstCatch = 0;
         success("Cleared!");
         return;
     }
 
-    error("Improper usage! /scc [clear, timer]");
+    error("Improper usage! /scc [reset, timer]");
 })
     .setName("seaCreatureCounter")
     .setAliases("scc");
@@ -308,12 +343,21 @@ function getSeaCreatureByMessage(message) {
     return null;
 }
 
-function formatFirstCatch() {
-    const date = new Date(firstCatch);
+function formatTimeFromMillis(milliseconds) {
+    const seconds = Math.floor(milliseconds / 1000) % 60;
+    const minutes = Math.floor(milliseconds / 1000 / 60) % 60;
+    const hours = Math.floor(milliseconds / 1000 / 60 / 60);
 
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
+    let formattedTime = "";
 
-    return `${hours}h ${minutes}m ${seconds}s`;
+    if (hours > 0) {
+        formattedTime += `${hours}h `;
+    }
+
+    if (minutes > 0 || hours > 0) {
+        formattedTime += `${minutes}m `;
+    }
+
+    formattedTime += `${seconds}s`;
+    return formattedTime;
 }
